7a5f367b808cbc3757723d9fe8f78f16a22b3d7e
Marcin Wołkowicz <mwolkowicz17@gmail.com>
2024-07-05 20:43:45 +0200

    pointer event

--- src/Box.jsx
+++ src/Box.jsx
@@ -19,7 +19,14 @@ export default function Box(props) {
 
     return (
         <>
-            <mesh{...props} ref={ref}>
+            <mesh{...props}
+                ref={ref}
+                onPointerDown={(e) => console.log('pointer down ' + e.object.name)}
+                onPointerUp={(e) => console.log('pointer up ' + e.object.name)}
+                onPointerOver={(e) => console.log('pointer over ' + e.object.name)}
+                onPointerOut={(e) => console.log('pointer out ' + e.object.name)}
+                onUpdate={(self) => console.log(self)}
+            >
                 <boxGeometry />
                 <meshBasicMaterial color={0x00ff00} wireframe />
             </mesh>
