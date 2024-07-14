cce16bbcf18964a74a3bd2a8a54ac350e769b794
Marcin Wołkowicz <mwolkowicz17@gmail.com>
2024-07-14 22:10:52 +0200

    stat

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 
 import { Canvas } from '@react-three/fiber'
 import Box from './Box'
+import { Stats } from '@react-three/drei'
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
       <Canvas camera={{ position: [0, 0, 2] }}>
         <Box position={[-0.75, 0, 0]} name="A" />
         <Box position={[0.75, 0, 0]} name="B" />
+        <Stats />
       </Canvas>
     </>
   )
